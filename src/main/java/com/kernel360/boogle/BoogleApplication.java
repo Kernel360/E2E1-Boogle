47d7680875c0771cf812dc7c6be12c1816261f37
Younglong <kyl8555@gmail.com>
2023-10-24 17:06:19 +0900

    chore: project init

---
+++ src/main/java/com/kernel360/boogle/BoogleApplication.java
@@ -0,0 +1,13 @@
+package com.kernel360.boogle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoogleApplication.class, args);
+	}
+
+}
