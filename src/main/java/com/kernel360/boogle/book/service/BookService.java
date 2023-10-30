740e956762f5b620350c134c8cfd75871e1ff1c9
Younglong <kyl8555@gmail.com>
2023-10-30 19:49:43 +0900

    refactor: 불필요한 파일 정리

--- src/main/java/com/kernel360/boogle/book/service/BookService.java
+++ src/main/java/com/kernel360/boogle/book/service/BookService.java
@@ -2,17 +2,15 @@ package com.kernel360.boogle.book.service;
 
 import com.kernel360.boogle.book.db.BookEntity;
 import com.kernel360.boogle.book.model.BookDTO;
-import com.kernel360.boogle.book.model.BookRequest;
 import com.kernel360.boogle.book.model.BookViewRequest;
 import org.springframework.data.domain.Page;
-import java.util.*;
+
+import java.util.Optional;
 
 public interface BookService {
     public void createBook(BookDTO book);
 
     public void updateBook(BookDTO book);
-
-    List<BookDTO> findAladinBook(BookRequest request);
 
     Optional<BookEntity> findById(Long bookId);
 
--- src/main/java/com/kernel360/boogle/book/service/BookServiceImpl.java
+++ src/main/java/com/kernel360/boogle/book/service/BookServiceImpl.java
@@ -3,7 +3,6 @@ package com.kernel360.boogle.book.service;
 import com.kernel360.boogle.book.db.BookEntity;
 import com.kernel360.boogle.book.db.BookRepository;
 import com.kernel360.boogle.book.model.BookDTO;
-import com.kernel360.boogle.book.model.BookRequest;
 import com.kernel360.boogle.book.model.BookViewRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,7 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.transaction.Transactional;
-import java.util.*;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -35,12 +34,6 @@ public class BookServiceImpl implements BookService {
     public void updateBook(BookDTO book) {
         book.getBookEntity().setLastModifiedBy("TEST"); // 로그인 사용자 정보 들어가야함
         bookRepository.save(book.getBookEntity());
-    }
-
-    @Override
-    public List<BookDTO> findAladinBook(BookRequest request) {
-
-        return null;
     }
 
     @Override
