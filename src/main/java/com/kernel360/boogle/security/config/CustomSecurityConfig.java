bd321a07622e4d97e963b44eb4705dfac57cb1fe
HyunJunSon <jjang3a@gmail.com>
2023-11-01 09:34:34 +0900

    feat: add login

---
+++ src/main/java/com/kernel360/boogle/security/config/CustomSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.kernel360.boogle.security.config;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Log4j2
+@Configuration
+@AllArgsConstructor
+@NoArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class CustomSecurityConfig {
+
+    private String name;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        log.info("----------------configure----------------------");
+
+        http.formLogin().loginPage("http://localhost:8080/admin/getAllBooks");
+
+        http.csrf().disable();
+
+        return http.build();
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("----------------configure----------------------");
+        return web -> web.ignoring().requestMatchers(PathRequest.
+                toStaticResources().atCommonLocations());
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/kernel360/boogle/security/service/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.kernel360.boogle.security.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private PasswordEncoder passwordEncoder;
+
+    public CustomUserDetailsService(){
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("loadUserByUsername: " + username);
+
+        return User.builder().username("user1")
+//                .password("1111")
+                .password(passwordEncoder.encode("1111"))
+                .authorities("ROLE_USER")
+                .build();
+    }
+
+
+}
