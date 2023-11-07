21bad94e3a06f632b9df11eb786c4264d6c187fa
GBGreenBravo <gbgreenbravo@gmail.com>
2023-11-07 22:46:23 +0900

    feat: Reply(댓글/대댓글 기능) ReplyResponse 추가

---
+++ src/main/java/com/kernel360/boogle/reply/model/ReplyResponse.java
@@ -0,0 +1,70 @@
+package com.kernel360.boogle.reply.model;
+
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public record ReplyResponse(
+        Long id,
+        Long memberId,
+        Long bookReportId,
+        String bookReportCreatedBy,
+        String content,
+        Long parentReplyId,
+        Set<ReplyResponse> childReplies,
+        LocalDateTime createdAt,
+        LocalDateTime lastModifiedAt,
+        String isDeleted
+) {
+    private static ReplyResponse of(Long id, Long memberId, Long bookReportId, String bookReportCreatedBy, String content, Long parentReplyId, LocalDateTime createdAt, LocalDateTime lastModifiedAt, String isDeleted) {
+        Comparator<ReplyResponse> childRepliesComparator = Comparator
+                .comparing(ReplyResponse::createdAt)
+                .thenComparingLong(ReplyResponse::id);
+        return new ReplyResponse(id, memberId, bookReportId, bookReportCreatedBy, content, parentReplyId, new TreeSet<>(childRepliesComparator), createdAt, lastModifiedAt, isDeleted);
+    }
+
+    public static ReplyResponse from(ReplyDTO replyDTO) {
+        Long bookReportId = replyDTO.getBookReportEntity().getId();
+        String bookReportCreatedBy = replyDTO.getBookReportEntity().getCreatedBy();
+
+        return ReplyResponse.of(
+                replyDTO.getId(),
+                replyDTO.getMemberId(),
+                bookReportId,
+                bookReportCreatedBy,
+                replyDTO.getContent(),
+                replyDTO.getParentReplyId(),
+                replyDTO.getCreatedAt(),
+                replyDTO.getLastModifiedAt(),
+                replyDTO.getIsDeleted()
+        );
+    }
+
+    public static Set<ReplyResponse> organizeChildReplies(List<ReplyDTO> dtos) {
+        Map<Long, ReplyResponse> map = dtos.stream()
+                .map(ReplyResponse::from)
+                .collect(Collectors.toMap(ReplyResponse::id, Function.identity()));
+
+        map.values().stream()
+                .filter(ReplyResponse::hasParentReply)
+                .forEach(replyResponse -> {
+                    ReplyResponse parentReply = map.get(replyResponse.parentReplyId());
+                    parentReply.childReplies().add(replyResponse);
+                });
+
+        return map.values().stream()
+                .filter(replyResponse -> !replyResponse.hasParentReply())
+                .collect(Collectors.toCollection(() ->
+                        new TreeSet<>(Comparator
+                                .comparing(ReplyResponse::createdAt)
+                                .reversed()
+                                .thenComparingLong(ReplyResponse::id)
+                        )
+                ));
+    }
+
+    public boolean hasParentReply() {
+        return parentReplyId != null;
+    }
+}
