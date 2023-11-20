71d181d91df3165a61dbf137dd8d870a161b5224
GBGreenBravo <gbgreenbravo@gmail.com>
2023-11-20 15:53:52 +0900

    refactor: custom error의 status를 HttpStatus.BAD_REQUEST.value()와 같은 형식으로 작성하도록 변경

--- src/main/java/com/kernel360/boogle/global/error/code/MemberErrorCode.java
+++ src/main/java/com/kernel360/boogle/global/error/code/MemberErrorCode.java
@@ -1,11 +1,12 @@
 package com.kernel360.boogle.global.error.code;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum MemberErrorCode implements ErrorCode {
-    NOT_FOUND_MEMBER(404, "회원을 찾을 수 없습니다."),
-    ALREADY_SIGNED_UP_MEMBER(409, "이미 등록된 유저입니다.");
+    NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND.value(), "회원을 찾을 수 없습니다."),
+    ALREADY_SIGNED_UP_MEMBER(HttpStatus.CONFLICT.value(), "이미 등록된 유저입니다.");
 
     private final int status;
     private final String message;
--- src/main/java/com/kernel360/boogle/global/error/code/ReplyErrorCode.java
+++ src/main/java/com/kernel360/boogle/global/error/code/ReplyErrorCode.java
@@ -1,10 +1,11 @@
 package com.kernel360.boogle.global.error.code;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum ReplyErrorCode implements ErrorCode {
-    EMPTY_CONTENT_REPLY(401, "공백은 입력될 수 없습니다.");
+    EMPTY_CONTENT_REPLY(HttpStatus.BAD_REQUEST.value(), "공백은 입력될 수 없습니다.");
 
     private final int status;
     private final String message;
