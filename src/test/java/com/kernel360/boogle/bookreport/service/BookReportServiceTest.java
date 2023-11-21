6e1ea4b88a54fa7f3117a0d477fa611e4af8570e
GBGreenBravo <gbgreenbravo@gmail.com>
2023-11-21 14:56:23 +0900

    refactor: BookReport(독후감) MemberEntity 연결로 인한 BookReportTest 수정

--- src/test/java/com/kernel360/boogle/bookreport/service/BookReportServiceTest.java
+++ src/test/java/com/kernel360/boogle/bookreport/service/BookReportServiceTest.java
@@ -4,11 +4,13 @@ import com.kernel360.boogle.book.db.BookRepository;
 import com.kernel360.boogle.bookreport.db.BookReportEntity;
 import com.kernel360.boogle.bookreport.db.BookReportRepository;
 import com.kernel360.boogle.bookreport.model.BookReportDTO;
+import com.kernel360.boogle.member.db.MemberEntity;
+import com.kernel360.boogle.member.db.MemberRepository;
+import com.kernel360.boogle.member.model.MemberDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -28,14 +30,19 @@ class BookReportServiceTest {
     private BookReportRepository bookReportRepository;
 
     @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
     private BookReportService bookReportService;
 
     @Test
     void createBookReport() {
         long rowCount = bookReportRepository.count();
+        MemberEntity member = memberRepository.findById(1L).get();
         BookReportDTO bookReportDTO = BookReportDTO.builder()
                 .bookReportEntity(
                         BookReportEntity.builder()
+                                .memberEntity(member)
                                 .bookEntity(bookRepository.findById(54L).get())
                                 .isPublic("N")
                                 .title("타이틀1")
@@ -43,7 +50,7 @@ class BookReportServiceTest {
                                 .build()
                 )
                 .build();
-        bookReportService.createBookReport(bookReportDTO);
+        bookReportService.createBookReport(bookReportDTO, MemberDTO.from(member));
         assertEquals(bookReportRepository.count(), rowCount + 1);
     }
 
@@ -87,6 +94,7 @@ class BookReportServiceTest {
 
     @Test
     void updateBookReport() {
+        MemberEntity member = memberRepository.findById(1L).get();
         BookReportDTO bookReportDTO = BookReportDTO.builder()
                 .bookReportEntity(
                     BookReportEntity.builder()
@@ -98,7 +106,7 @@ class BookReportServiceTest {
                         .build()
                 )
                 .build();
-        bookReportService.updateBookReport(bookReportDTO);
+        bookReportService.updateBookReport(bookReportDTO, MemberDTO.from(member));
         assertThat(bookReportRepository.findById(1L).get().getIsPublic()).isEqualTo("N");
     }
 
@@ -106,6 +114,6 @@ class BookReportServiceTest {
     void deleteBookReport() {
         Long bookReportId = 1L;
         bookReportService.deleteBookReport(bookReportId);
-        assertThat(bookReportRepository.findById(bookReportId).get().getIsDeleted()).isEqualTo("Y");
+        assertThat(bookReportRepository.findById(bookReportId).isEmpty());
     }
 }
