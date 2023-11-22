e851afd000780193f99014e7e846ac114ff8eb3c
HyunJunSon <jjang3a@gmail.com>
2023-11-22 18:27:11 +0900

    [feat] 화면수정

--- src/main/java/com/kernel360/boogle/mypage/controller/MyPageController.java
+++ src/main/java/com/kernel360/boogle/mypage/controller/MyPageController.java
@@ -1,6 +1,5 @@
 package com.kernel360.boogle.mypage.controller;
 
-import com.kernel360.boogle.book.service.BookService;
 import com.kernel360.boogle.bookreport.service.BookReportService;
 import com.kernel360.boogle.member.db.MemberEntity;
 import com.kernel360.boogle.member.service.MemberService;
@@ -11,7 +10,6 @@ import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Api(tags = {"마이페이지 관련 API"})
@@ -26,55 +25,41 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Slf4j
 public class MyPageController {
-    private final BookService bookService;
+
     private final BookReportService bookReportService;
     private final ReplyService replyService;
     private final MemberService memberService;
     private final MyPageService myPageService;
 
     @GetMapping("/mypage/main")
-    public ModelAndView getMyPageMain(@AuthenticationPrincipal MemberEntity member){
+    public ModelAndView getMyPageMain(@AuthenticationPrincipal MemberEntity member) {
         final Long memberId = member.getId();
 
         return new ModelAndView("mypage/main")
                 .addAllObjects(
                         Map.of("member", member,
-                                "replies" , replyService.getRecentRepliesByMemberId(memberId,10),
+                                "replies", replyService.getRecentRepliesByMemberId(memberId, 10),
                                 "bookReports", bookReportService.getBookReportsByMemberId(memberId))
                 );
     }
+
     @GetMapping("/mypage/memberInfo")
     public ModelAndView getMemberInfo(
-            @AuthenticationPrincipal MemberEntity member){
+            @AuthenticationPrincipal MemberEntity member) {
 
-        return new ModelAndView("mypage/memberInfo" )
+        return new ModelAndView("mypage/memberInfo")
                 .addObject("member", member);
     }
 
-    @GetMapping("/mypage/bookReport")
-    public ModelAndView getBookReport(
-            @AuthenticationPrincipal MemberEntity member
-    ){
-        return new ModelAndView("mypage/bookReport")
-                .addObject("bookReport", bookReportService.getBookReportsByMemberId(member.getId()));
-    }
-
-    @GetMapping("/mypage/communityReply")
-    public ModelAndView getCommunityReply(@AuthenticationPrincipal MemberEntity member){
-
-        return new ModelAndView("/mypage/communityReply")
-                .addObject("replies",
-        replyService.getRecentRepliesByMemberId(member.getId(), replyService.getAllRepliesCount(member.getId())));
-
-    }
 
     @PostMapping("/mypage/memberInfo")
     public ModelAndView updateMemberInfo(
             @AuthenticationPrincipal MemberEntity memberEntity,
-            @Validated
+            @Valid
             @ModelAttribute MemberRequestDTO member,
+
             RedirectAttributes redirectAttributes
-    ){
+    ) {
         ModelAndView modelAndView = new ModelAndView();
 
         if (myPageService.validatePassword(memberEntity, member)) {
--- src/main/java/com/kernel360/boogle/mypage/model/MemberRequestDTO.java
+++ src/main/java/com/kernel360/boogle/mypage/model/MemberRequestDTO.java
@@ -3,16 +3,39 @@ package com.kernel360.boogle.mypage.model;
 
 import lombok.*;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @Builder
-@Data
 public class MemberRequestDTO {
 
+    @NotBlank(message = "이름은 의무 입력입니다.")
+    @Size(min = 2, max = 5, message = "이름은 2~5자 사이여야 합니다.")
     private String name;
+
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Pattern(regexp = "^[0-9a-zA-Z]{6,}$",
+            message = "비밀번호는 최소 6자 이상이어야 하며, 숫자나 영어만 포함해야 합니다.")
     private String password;
+
+    @Email(message = "이메일 형식이 아닙니다.")
     private String email;
+
+    @Size(min = 2, max = 10, message = "닉네임은 2 ~ 10 글자만 가능합니다.")
     private String nickname;
+
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "전화번호형식은 000-0000-0000 입니다.")
     private String phoneNumber;
 
 }
+
+
+
+
+
