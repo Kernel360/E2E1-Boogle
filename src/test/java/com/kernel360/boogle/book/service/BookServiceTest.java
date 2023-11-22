3c9e4e6f488ac302ed2d5a36a1c3c9a27d6f57f8
Wonsang Kim <rosskysoss@gmail.com>
2023-11-22 10:22:55 +0900

    feat: test code for book-repository

---
+++ src/test/java/com/kernel360/boogle/book/service/BookServiceTest.java
@@ -0,0 +1,83 @@
+package com.kernel360.boogle.book.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class BookServiceTest {
+
+    @Test
+    void createBook() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getBookById() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getBooks() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getBooksByAuthor() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getBooksByTitle() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void getBooksByPublisher() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void updateBook() {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    void deleteBook() {
+        // given
+
+        // when
+
+        // then
+    }
+}
